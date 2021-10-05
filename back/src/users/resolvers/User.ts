c59ab8f82b74d60635a4ed5a6373854196575511
Laurent Paoletti <lp@providenz.fr>
2021-10-05 15:41:13 +0000

    Avoid requesting sirene api for user companies

--- back/src/users/resolvers/User.ts
+++ back/src/users/resolvers/User.ts
@@ -1,8 +1,8 @@
 import { convertUrls, getInstallation } from "../../companies/database";
 import { UserResolvers, CompanyPrivate } from "../../generated/graphql/types";
 import { getUserCompanies } from "../database";
-import { searchCompany } from "../../companies/sirene";
 
+import { nafCodes } from "./naf";
 const userResolvers: UserResolvers = {
   // Returns the list of companies a user belongs to
   // Information from TD, Sirene, and s3ic are merged
@@ -12,13 +12,11 @@ const userResolvers: UserResolvers = {
     return Promise.all(
       companies.map(async company => {
         let companyPrivate: CompanyPrivate = convertUrls(company);
-        try {
-          // try to set naf, libelleNaf and address from SIRENE database
-          const { naf, libelleNaf, address } = await searchCompany(
-            company.siret
-          );
-          companyPrivate = { ...companyPrivate, naf, libelleNaf, address };
-        } catch {}
+
+        const { codeNaf: naf, address } = company;
+        const libelleNaf = naf in nafCodes ? nafCodes[naf] : "";
+
+        companyPrivate = { ...companyPrivate, naf, libelleNaf, address };
 
         // retrieves associated ICPE
         const installation = await getInstallation(company.siret);
